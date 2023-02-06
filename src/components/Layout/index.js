1f2411662e4c95c58cf2f90bae5c4b491714d1a1
oladetoungee <demilad1998@gmail.com>
2023-02-06 23:53:00 +0100

    first commit

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Sidebar/'
+import './index.scss'
+
+const Layout = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <h1>KJNMJKNMKJ</h1>
+    <a>Jnbsdjn</a>
+     
+    </div>
+  )
+}
+
+export default Layout
