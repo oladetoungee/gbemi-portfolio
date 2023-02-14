b62ff73f7eeb6c48e35c437702ba8adcca92bbd5
oladetoungee <demilad1998@gmail.com>
2023-02-14 23:54:49 +0100

    home page update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import Layout from './components/Layout'
 import Sidebar from './components/Sidebar'
+import Home from './components/Home'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route path="/HOME" element={<Home />} />
       </Routes>
     </>
   )
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import LogoTitle from '../../assets/images/logoS.png'
+
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+            <h1>This is home</h1>
+            <img src={LogoTitle} alt="Letter G logo"/>
+            </div>
+        
+        </div>
+    )
+}
+export default Home
