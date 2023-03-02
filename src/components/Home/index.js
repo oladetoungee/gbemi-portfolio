e31fb86634c619a0487ac273d3bf73b5cce2de92
oladetoungee <demilad1998@gmail.com>
2023-03-03 00:16:07 +0100

    home page animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,30 +5,43 @@ import './index.scss'
 import { useEffect, useState } from 'react'
 
 const Home = () => {
-const [letterClass, setletterClass] = useState('text-animate')
-const nameArray = ['G', 'B', 'E', 'M', 'I']
-useEffect(() => {
-    return setTimeout(() => {
-        setletterClass('text-animate-hover')
+  const [letterClass, setletterClass] = useState('text-animate')
+  const nameArray = ['G', 'B', 'E', 'M', 'I']
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setletterClass('text-animate-hover')
     }, 4000)
-}, [])
-    return (
- 
-        <div className="container home-page">
-            <div className="text-zone">
-            <h1>Hello, <img src={LogoTitle} alt="Letter G logo"/>  <br/> I'm 
-            <AnimatedLetters letterClass={letterClass}
-            strArray={nameArray} idx={15}
-            />
-            <br/>
-       
-            </h1>
-            <h2>Frontend Developer / Technical Writer / Vue Js / React Js</h2>
-            <Link to="/contact" className="flat-button">CONTACT ME</Link>
-            </div>
-        
-        </div>
 
-    )
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hello, <img src={LogoTitle} alt="Letter G logo" /> <br /> I'm
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+        </h1>
+        <h2>Frontend Developer / Technical Writer / Vue Js / React Js</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+      <div className="image-zone">
+        <div className="content">
+        <img src={'https://abdelrahman345.netlify.app/images/me.jpg'} alt="Letter G logo" /> 
+      <h2>Gbemisola Oladetoun <span>Frontend Dev</span></h2>
+      <a href={'https://abdelrahman345.netlify.app/'}></a> 
+        </div>
+    
+      </div>
+    </div>
+  )
 }
 export default Home
---
+++ src/components/Logo/index.js
@@ -0,0 +1,8 @@
+import './index.scss'
+
+const Logo = () => {
+    retutn (
+<div>HELLO</div>
+    )
+}
+export default Logo
